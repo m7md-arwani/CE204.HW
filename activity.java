9d79ba7a764dff3712947ce660adc5848bbf3597
Arwani <mohammad.arwani@ogr.altinbas.edu.tr>
2023-08-25 15:23:26 +0300

    Putting the "stu" class in a separate file.

--- activity.java
+++ activity.java
@@ -87,36 +87,3 @@ public class activity {
 
 
 
-class stu {
-
-    private String name ; 
-    private String address ; 
-
-public stu( String name, String address) {
-
-   
-    this.name = name ; 
-    this.address = address ; 
-}
-
-
-
-public String getName() {
-
-    return this.name ; 
-}
-
-public String getAddress() {
-    return this.address ; 
-}
-
-
-public void setName(String nm) {
-    this.name = nm ; 
-}
-public void setAddress(String add) {
-    this.address = add ; 
-}
-
-
-}
---
+++ stu.java
@@ -0,0 +1,33 @@
+public class stu {
+
+    private String name ;
+    private String address ;
+
+    public stu( String name, String address) {
+
+
+        this.name = name ;
+        this.address = address ;
+    }
+
+
+
+    public String getName() {
+
+        return this.name ;
+    }
+
+    public String getAddress() {
+        return this.address ;
+    }
+
+
+    public void setName(String nm) {
+        this.name = nm ;
+    }
+    public void setAddress(String add) {
+        this.address = add ;
+    }
+
+
+}
